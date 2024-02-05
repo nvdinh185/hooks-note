2b361d38a8add89e957e9996a082933e34857929
nvdinh185 <nvdinh0766777123@gmail.com>
2024-02-05 18:28:43 +0700

    hooks

--- src/components/NoteComponent.js
+++ src/components/NoteComponent.js
@@ -1,29 +1,24 @@
-import { Component } from 'react';
+const Note = (props) => {
 
-class Note extends Component {
-
-    onClickToEdit(props) {
-        this.props.onCallEditNote(props);
+    const onClickToEdit = (props) => {
+        props.onCallEditNote(props);
     }
 
-    onClickToDel(id) {
-        this.props.onDelNote(id);
+    const onClickToDel = (id) => {
+        props.onDelNote(id);
     }
 
-
-    render() {
-        const bg = this.props.category === "Must do" ? "bg-danger" : this.props.category === "Could do" ? "bg-warning" : "bg-primary";
-        const classCss = `container border wrapper float ${bg}`;
-        return (
-            <div className={classCss}>
-                <h4>{this.props.category}</h4>
-                <p>{this.props.content}</p>
-                <p>{this.props.time}</p>
-                <button className='btn-outline-light btn-success text-dark' onClick={() => this.onClickToEdit(this.props)}>Edit</button>
-                <button className='btn-outline-light btn-secondary text-dark' onClick={() => this.onClickToDel(this.props.id)}>Del</button>
-            </div>
-        )
-    }
+    const bg = props.category === "Must do" ? "bg-danger" : props.category === "Could do" ? "bg-warning" : "bg-primary";
+    const classCss = `container border wrapper float ${bg}`;
+    return (
+        <div className={classCss}>
+            <h4>{props.category}</h4>
+            <p>{props.content}</p>
+            <p>{props.time}</p>
+            <button className='btn-outline-light btn-success text-dark' onClick={() => onClickToEdit(props)}>Edit</button>
+            <button className='btn-outline-light btn-secondary text-dark' onClick={() => onClickToDel(props.id)}>Del</button>
+        </div>
+    )
 }
 
 export default Note
